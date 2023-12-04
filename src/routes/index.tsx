2483e209e2c1cc817747945bc84f2f212d7e357f
Cheleste1994 <wolverines@tut.by>
2023-12-04 12:22:54 +0300

    feat: add MUI in dependencies

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import React, { lazy, Suspense } from 'react';
 import { Routes, Route, Outlet } from 'react-router-dom';
+import Header from '../components/Header/Header';
 import LogoLoad from '../components/LogoLoad/LogoLoad';
 import PageError from './404/PageError';
 
@@ -17,6 +18,7 @@ const Router = (): React.JSX.Element => (
             </main>
           }
         >
+          <Header />
           <Outlet />
         </Suspense>
       }
