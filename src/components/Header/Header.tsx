d5fcac160b013d018e7e1e356ee0cc078b472033
Cheleste1994 <wolverines@tut.by>
2023-12-04 12:54:25 +0300

    feat: add material icons in dependencies

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,20 @@
+import { Badge } from '@mui/material';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import React from 'react';
 import styles from './Header.module.scss';
+import { Link } from 'react-router-dom';
 
 export default function Header(): JSX.Element {
   return (
     <header className={styles.header}>
-      <div>React. Components</div>
-      <div></div>
+      <div>
+        <Link to={'/'}>Tagging Notes</Link>
+      </div>
+      <Link to={'/favorite'}>
+        <Badge badgeContent={5} color="secondary">
+          <FavoriteBorderIcon />
+        </Badge>
+      </Link>
     </header>
   );
 }
